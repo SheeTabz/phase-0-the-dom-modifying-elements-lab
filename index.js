337d1432adc75f33bbd727ef4f47d0a70df6299e
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-20 11:47:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 // Write your code here!
-// const element = document.createElement("div")
-// document.body.append(element)
-// const ul = document.createElement("ul");
+const main = document.getElementById('main');
+main.remove()
 
-// for (let i = 0; i < 3; i++) {
-//     const li = document.createElement("li");
-//     li.textContent = (i + 1).toString();
-//     ul.append(li);
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Tabitha is the champion'
+
+
+
+
+
+
+
+
+// const element = document.createElement('div');
+// element.className = "div1"
+// const body = document.querySelector('body')
+// body.append(element)
+// const ul = document.createElement('ul')
+// // console.log(ul.innerHTML)
+// for(let i = 0; i < 3; i++) {
+//     const li = document.createElement('li')
+//     li.textContent = (i + 1).toString()
+//     ul.append(li)
 // }
+// element.append(ul)
 
-// element.append(ul);
 
-// const main = document.getElementById("main");
-// main.innerHTML =
-//     "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"
-//     element.style.height = "300px";
-// element.style.backgroundColor = "#27647B";
-// element.textContent = "You've changed what's on the screen!";
-const body = document.querySelector("body");
-const main = document.getElementById("main");
-main.remove();
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory")
-newHeader.textContent="YOUR-NAME is the champion"
-body.append(newHeader)
